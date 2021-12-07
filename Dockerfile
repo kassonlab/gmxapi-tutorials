30bd0b07d1bb444a5b5b7fa3bf61d8d2ccc90399
M. Eric Irrgang <ericirrgang@gmail.com>
2021-12-07 13:57:51 +0300

    [wip] Updated example.
    Work in progress with some error corrections and workarounds.
    
    Note that the `rms` command fails, unable to read the input.

---
+++ Dockerfile
@@ -0,0 +1,91 @@
+# Provide an easy-to-reproduce environment in which to test full Python functionality.
+
+# Build (requires Docker 17.05 or higher):
+#     docker build -t gmxapi-tutorial .
+#
+# Run an interactive shell:
+#     docker run --rm -ti gmxapi-tutorial
+#     . venv/bin/activate
+#     cd examples
+#     python
+#     from figure1 import *
+#     make_top = figure1a()
+#     ...
+
+FROM gmxapi/gromacs-dependencies-mpich:2022b2 as python-base
+
+RUN apt-get update && \
+    DEBIAN_FRONTEND=noninteractive apt-get -yq --no-install-suggests --no-install-recommends \
+    install \
+        python3 \
+        python3-dev \
+        python3-venv && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN groupadd -r tutorial && useradd -m -s /bin/bash -g tutorial tutorial
+
+USER tutorial
+WORKDIR /home/tutorial
+
+ENV VENV /home/tutorial/venv
+RUN python3 -m venv $VENV
+RUN . $VENV/bin/activate && \
+    pip install --no-cache-dir --upgrade pip setuptools wheel
+
+FROM gmxapi/gromacs-mpich:2022b2 as gromacs
+
+FROM python-base
+
+COPY --from=gromacs /usr/local/gromacs /usr/local/gromacs
+
+RUN $VENV/bin/python -m pip install --upgrade pip setuptools wheel
+
+RUN $VENV/bin/pip install mpi4py
+
+ARG GMXAPI_URL="https://drive.google.com/uc?export=download&id=1-n-b6hmUNjaF9h4VBw0SkCKQbUIAvDsL"
+
+RUN . $VENV/bin/activate && \
+    wget --no-check-certificate $GMXAPI_URL -O gmxapi-0.3.0b2.tgz && \
+    . /usr/local/gromacs/bin/GMXRC && \
+    pip install ./gmxapi-0.3.0b2.tgz && \
+    python -c 'import gmxapi' && \
+    rm ./gmxapi-0.3.0b2.tgz
+
+ARG BRER_URL="https://github.com/kassonlab/brer_plugin/archive/master.tar.gz"
+
+RUN . $VENV/bin/activate && \
+    wget $BRER_URL -O brer-plugin-master.tar.gz && \
+    tar zxvf brer-plugin-master.tar.gz && \
+    cd brer_plugin-master && \
+    mkdir build && \
+    cd build && \
+    gmxapi_DIR=/usr/local/gromacs cmake .. && \
+    cmake --build . --target install && \
+    cd .. && \
+    cd .. && \
+    python -c 'import brer' && \
+    rm -rf brer*
+
+ARG RUN_BRER_URL="https://github.com/kassonlab/run_brer/archive/master.tar.gz"
+
+RUN . $VENV/bin/activate && \
+    wget $RUN_BRER_URL -O run-brer-master.tar.gz && \
+    pip install ./run-brer-master.tar.gz && \
+    python -c 'import run_brer' && \
+    rm -rf run-brer-master.tar.gz
+
+ARG PEPTIDE_INPUTS="https://drive.google.com/uc?export=download&id=1eNBBdGQ8fjbaaAG6kQMBcVnhb7aRFmQ6"
+
+RUN mkdir $HOME/input && \
+    cd $HOME/input && \
+    wget $PEPTIDE_INPUTS -O fs-peptide.tgz && \
+    tar zxvf fs-peptide.tgz
+
+ADD --chown=tutorial:tutorial examples /home/tutorial/examples
+
+ENV INPUT_DIR=/home/tutorial/input
+
+CMD /home/tutorial/venv/bin/python /home/tutorial/examples/figure1.py
+
+# MPI tests can be run in this container without requiring MPI on the host.
+# (We suggest running your docker engine with multiple CPU cores allocated.)
--- examples/fs-peptide.py
+++ examples/fs-peptide.py
@@ -1,24 +1,27 @@
 """Complete working example for figure 1.
 
 Prerequisites:
-    This script assumes it is running in a local copy of the
-    https://github.com/kassonlab/gmxapi-tutorials repository. It will look for input
-    files relative to the location of this file.
+    Make sure the input files are available locally. The script will first look for an
+    INPUT_DIR environment variable before defaulting to `./input/` for the directory
+    `fs-peptide`
 
 After installing GROMACS and gmxapi, execute this script with a Python 3.7+ interpreter.
 
 For a trajectory ensemble, use `mpiexec` and `mpi4py`. For example, for an ensemble of
 size 50, activate your gmxapi Python virtual environment and run
 
-    mpiexec -n 50 `which python` -m mpi4py fs-peptide.py
+    mpiexec -n 50 `which python` -m mpi4py figure1.py
 
 """
 import logging
 import os
+import typing
 from pathlib import Path
 
 import gmxapi as gmx
-from gmxapi import logical_not
+import gmxapi.abc
+from gmxapi import function_wrapper
+from gmxapi.exceptions import UsageError
 
 logging.basicConfig(level=logging.INFO)
 
@@ -36,13 +39,78 @@ try:
 except:
     ensemble_size = 1
 
+#
+# Define some helpers
+#
+
+
+def less_than(lhs: typing.SupportsFloat, rhs: typing.SupportsFloat):
+    """Compare the left-hand-side to the right-hand-side.
+
+    Follows the Numpy logic for normalizing the numeric types of *lhs* and *rhs*.
+    """
+    import numpy as np
+    dtype = int
+    if any(isinstance(operand, float) for operand in (lhs, rhs)):
+        dtype = float
+    elif all(isinstance(operand, typing.SupportsFloat) for operand in (lhs, rhs)):
+        if type(np.asarray([lhs, rhs])[0].item()) is float:
+            dtype = float
+    elif any(isinstance(operand, gmxapi.abc.Future) for operand in (lhs, rhs)):
+        for item in (lhs, rhs):
+            if hasattr(item, 'dtype'):
+                if issubclass(item.dtype, (float, np.float)):
+                    dtype = float
+            elif hasattr(item, 'description'):
+                if issubclass(item.description.dtype, (float, np.float)):
+                    dtype = float
+    else:
+        raise UsageError(f'No handling for [{repr(lhs)}, {repr(rhs)}].')
+
+    if dtype is int:
+        def _less_than(lhs: int, rhs: int) -> bool:
+            return lhs < rhs
+    elif dtype is float:
+        def _less_than(lhs: float, rhs: float) -> bool:
+            return lhs < rhs
+    else:
+        raise UsageError('Operation only supports standard numeric types.')
+    return function_wrapper()(_less_than)(lhs=lhs, rhs=rhs)
+
+
+@function_wrapper()
+def _numpy_min_float(data: gmx.NDArray) -> float:
+    import numpy as np
+    logging.info(f'Looking for minimum in {data}')
+    return float(np.min(data._values))
+
+
+def numeric_min(array):
+    """Find the minimum value in an array.
+    """
+    return _numpy_min_float(data=array)
+
+
+@gmx.function_wrapper(output={'data': gmx.NDArray})
+def xvg_to_array(path: str, output):
+    """Get an NxM array from a GROMACS xvg data file.
+
+    For energy output, N is the number of samples, and the first of M
+    columns contains simulation time values.
+    """
+    import numpy
+    logging.info(f'Reading xvg file {path}.')
+    data = numpy.genfromtxt(path, comments='@', skip_header=14)
+    logging.info(f'Read array shape {data.shape} from {path}.')
+    output.data = data
+
 
 ######################
 # Confirm inputs exist
 #
 
-_script_dir = Path(__file__)
-input_dir = _script_dir.parent.parent.resolve() / 'input' / 'fs-peptide'
+input_dir = os.getenv('INPUT_DIR', default=os.path.join('.', 'input'))
+input_dir = Path(input_dir) / 'fs-peptide'
 if not all(p.exists() for p in (input_dir, input_dir / 'start0.pdb', input_dir / 'ref.pdb')):
     raise RuntimeError('Missing input files.')
 reference_struct = input_dir / 'ref.pdb'
@@ -104,7 +172,7 @@ def figure1b(make_top):
 
     input_list = gmx.read_tpr(tpr_input)
 
-    md = gmx.mdrun(input_list, runtime_args={'-maxh': '2'})
+    md = gmx.mdrun(input_list, runtime_args={'-maxh': str(0.001)})
     md.run()
 
     return {
@@ -114,30 +182,38 @@ def figure1b(make_top):
 
 def figure1c(input_list):
     """Figure 1c: looping and custom operations"""
-    subgraph = gmx.subgraph(variables={'found_native': False, 'checkpoint': '', 'min_rms': 1e6})
+    subgraph = gmx.subgraph(variables={'found_native': False, 'checkpoint': '',
+                                       'min_rms': 1e6, 'incomplete': True, 'iteration':
+                                           0})
     with subgraph:
-        md = gmx.mdrun(
-            input_list,
-            runtime_args={
-                '-cpi': subgraph.checkpoint,
-                '-maxh': '2'
-            })
+        # subgraph.iteration = subgraph.iteration + 1
+        logging.info(f'Iteration {subgraph.iteration}.')
+        md = gmx.mdrun(input_list, runtime_args={'-cpi': subgraph.checkpoint, '-maxh':
+            str(.001)})
+        logging.info(f'Binding checkpoint {md.output.checkpoint}')
         subgraph.checkpoint = md.output.checkpoint
+        logging.info(f'Setting up rmsd calculation.')
         rmsd = gmx.commandline_operation(
             'gmx', ['rms'],
             input_files={
                 '-s': reference_struct,
                 '-f': md.output.trajectory},
             output_files={'-o': 'rmsd.xvg'},
-            stdin='Backbone Backbone\n'
+            stdin='Backbone Backbone'
         )
+        logging.info(f'Binding output {rmsd.output.file}')
         subgraph.min_rms = numeric_min(
             xvg_to_array(rmsd.output.file['-o']).output.data).output.data
         subgraph.found_native = less_than(lhs=subgraph.min_rms, rhs=0.3).output.data
+        logging.info(f'Bound result {subgraph.found_native}')
+        subgraph.found_native = False
+        subgraph.incomplete = gmx.logical_not(subgraph.found_native)
 
     folding_loop = gmx.while_loop(operation=subgraph,
-                                  condition=logical_not(subgraph.found_native))()
+                                  condition=subgraph.incomplete)()
+    logging.info('Beginning folding_loop.')
     folding_loop.run()
+    logging.info(f'Finished folding_loop at iteration {subgraph.iteration}.')
     return folding_loop
 
 
